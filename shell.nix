223fdc041d15e686bd510b4a98ccfbfc80e4cdf9
Stefan Machmeier <stefan-machmeier@outlook.com>
2021-10-04 12:02:30 +0200

    Add shell nix

---
+++ shell.nix
@@ -0,0 +1,48 @@
+with import <nixpkgs> {};
+with luaPackages;
+
+let
+  libs = [
+      conntrack-tools
+      openssl
+      ninja
+      libpcap
+      lua5_1
+      luasocket
+      luasec
+      cjson
+      nettools
+      psutils
+      gcc
+      taglib
+      git
+      libxml2
+      libxslt
+      libzip
+      zlib
+      # Test
+      gdb
+      pkg-config
+      strace
+      valgrind
+  ];
+in
+stdenv.mkDerivation rec {
+  name = "madcat";
+  buildInputs = libs;
+  nativeBuildInputs = [ cmake ];
+  shellHook = ''
+    export LD_LIBRARY_PATH=$(nix eval --raw nixpkgs.lua5_1)/include:$LD_LIBRARY_PATH
+    export C_INCLUDE_PATH="$PWD/include:$PWD/include/rsp:$(nix eval --raw nixpkgs.lua5_1)/include"
+    #export LUA_CPATH="${lib.concatStringsSep ";" (map getLuaCPath libs)}"
+    #export LUA_PATH="${lib.concatStringsSep ";" (map getLuaPath libs)}"
+    # Check first if build directory exists already
+    if [ -d "build" ]; then
+      rm -rf build
+    fi
+    cmake -B build
+    cd build
+    ctest
+    cd ..
+  '';
+}
